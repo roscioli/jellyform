a8939dd9342fa96b728fba66a1ea9b13b98a44b5
roscioli <octavio.d.roscioli@vanderbilt.edu>
2021-02-19 13:03:04 -0500

    proper form updates

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 
 import Jellyform, {_FakeForm, _InputComponentProps, _fixture} from 'jellyform'
 import 'jellyform/dist/index.css'
 
 const App = () => {
-  return <Jellyform<_FakeForm, {}, _InputComponentProps> {..._fixture.getFormProps()} />
+  const initialProps = useMemo(() => _fixture.getFormProps(), [])
+  const [formValues, setFormValues] = useState(_fixture.getFormInitialState())
+
+  return <Jellyform<_FakeForm, {}, _InputComponentProps> {...initialProps} formValues={formValues} setForm={setFormValues} />
 }
 
 export default App
